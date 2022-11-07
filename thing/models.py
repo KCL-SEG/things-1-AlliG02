5dc451a43237b0fa9569cde2c8b92c48e2a438fd
Allister Gough <allistergough02@gmail.com>
2022-11-07 18:44:41 +0000

    fix problem

--- thing/models.py
+++ thing/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Thing(AbstractUser):
-    name = models.TextField
-    description = models.TextField
-    quantity = models.PositiveIntegerField
+    name = models.TextField()
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
