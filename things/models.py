711c2f56ef308114151b61f0b06f29b46baf1dd1
Allister Gough <allistergough02@gmail.com>
2022-11-07 19:36:48 +0000

    fix problems

--- things/models.py
+++ things/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
 
 # Create your models here.
-
-class Thing(AbstractUser):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
--- things/views.py
+++ things/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# home page
 def home(request):
     return render(request, 'home.html')
