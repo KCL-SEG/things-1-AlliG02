c5d0eb21783dd7c12512b2e676d7fd22c61d3532
Allister Gough <allistergough02@gmail.com>
2022-11-08 11:22:32 +0000

    fix problems

--- things/models.py
+++ things/models.py
@@ -6,6 +6,6 @@ from django.core.validators import MinValueValidator
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.TextField(max_length = 30, unique = True, blank = False)
-    description = models.TextField(max_length = 120, unique = False, blank = True)
+    name = models.CharField(max_length = 30, unique = True, blank = False)
+    description = models.CharField(max_length = 120, unique = False, blank = True)
     quantity = models.PositiveIntegerField(validators = [MinValueValidator(0), MaxValueValidator(100)])
